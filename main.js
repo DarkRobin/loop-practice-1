e01660aa32e96f5c7679d3cd07e638cc77ab7b9f
charles <sam.charles.harrod@gmail.com>
2015-01-19 13:52:40 -0700

    Finished

---
+++ main.js
@@ -0,0 +1,76 @@
+
+
+//DEMO
+//for(var i=0; i<5; i++){
+//	console.log(i);
+//}
+
+
+
+
+//Problem 1
+//for(var i=0; i<=500; i+=100){
+//	console.log(i);
+//
+//}
+
+
+//Problem 2
+//for(var i=1; i<=64; i*=2){
+//	console.log(i);
+//}
+
+
+//Problem 3
+// for(var i=1; i<=3; i++){
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+
+
+//Problem 4
+//for(var i=0; i<=10; i+=2){
+//	console.log(i);
+//}
+
+
+//Problem 5
+//for(var i=3; i<=15; i+=3){
+//	console.log(i);
+//
+//}
+
+
+//Problem 6
+//for(var i=9; i>=0; i-=1){
+//	console.log(i);
+//
+//}
+
+//Problem 7
+for(var i=0; i<3; i++){
+	for(var x=0; x<=3; x++)
+	console.log(x);
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
